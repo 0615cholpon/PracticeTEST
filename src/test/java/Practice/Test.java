1fcf7bdc2ec164151edd5ea8ef5ca5178a77bdf7
Cholpon Alymova <0615cholpon@gmail.com>
2019-09-07 19:56:35 -0500

    changes

--- src/test/java/Practice/Test.java
+++ src/test/java/Practice/Test.java
@@ -3,5 +3,6 @@ package Practice;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("HELLO WORLD BY TEST");
     }
 }
